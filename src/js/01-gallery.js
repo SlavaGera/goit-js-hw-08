4a6612396571d0e3cb870a9d1bbf37986e92e463
Slavik Herasiuta <theprodigy77@gmail.com>
2022-10-23 20:12:58 +0300

    add 3 task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,20 @@
 // Add imports above this line
+import SimpleLightbox from "simplelightbox";
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryRef = document.querySelector('.gallery');
+
+const markup = galleryItems.map(({ preview, original, description }) =>
+    `<div class="gallery__item">
+    <a class='gallery__link' href = '${original}'>
+    <img class='gallery__image' src='${preview}' data-source='${original}' alt ='${description}'/>
+    </a>
+    </div>`).join('');
+
+galleryRef.insertAdjacentHTML('beforeend', markup);
+
+let lightbox = new SimpleLightbox('.gallery a', {captionsData: 'alt', captionDelay: 250, captionPosition: 'bottom' });
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,42 @@
+import throttle from 'lodash.throttle';
 
+const refs = {
+    form: document.querySelector('.feedback-form'),
+    email: document.querySelector('input'),
+    textarea: document.querySelector('textarea')
+};
+
+const STORAGE_KEY = "feedback-form-state";
+
+let formData = {};
+
+refs.form.addEventListener('input', throttle(onFormInput, 500));
+refs.form.addEventListener('submit', onFormSubmit);
+
+populateTextForm();
+
+function onFormInput(event) {
+    formData[event.target.name] = event.target.value;
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+};
+
+function onFormSubmit(event) {
+    event.preventDefault();
+    localStorage.removeItem(STORAGE_KEY);
+    event.currentTarget.reset();
+};
+
+function populateTextForm(event) {
+    const saveMsg = localStorage.getItem(STORAGE_KEY);
+    const outputTxt = JSON.parse(saveMsg);
+
+    if (saveMsg) {
+        refs.email.value = outputTxt.email;
+        refs.textarea.value = outputTxt.message;
+    }
+
+    formData = {
+        email: outputTxt.email,
+        message: outputTxt.message
+    }
+};
